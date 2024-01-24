83b7f34ea836ba6a5be6d9e11883467a490f2e33
CoderGirllll <131524016+CoderGirllll@users.noreply.github.com>
2024-01-24 19:57:31 +0530

    Revised Python Language (Binary)
    Revised data handling for binary files in python language.

---
+++ Lang_Recalls/python/File_Handling/bin_tuples.py
@@ -0,0 +1,21 @@
+import pickle
+#Storing records as list
+f = open("Bin_Tuples.dat", "wb")
+Rec = []
+while True:
+    roll = int(input("Roll No.: "))
+    name = input("Enter name: ")
+    R = [roll, name]
+    Rec.append(R)
+    ch = input("Do you want to enter more records? (y/n)")
+    if ch in 'nN':
+        break
+    pickle.dump(Rec, f) #Dumps the entire list/ Writing binary file
+    f.close()
+
+#Content not dumping in the binary file
+
+fr = open("Bin_Tuples.dat", "rb")
+read = pickle.load(fr) #Loads the entire list/ Reading binary file
+print(read)
+fr.close()
