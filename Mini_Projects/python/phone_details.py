c27c0524abb60d0060b26f78f9f0e8219e84421c
CoderGirllll <131524016+CoderGirllll@users.noreply.github.com>
2024-07-26 16:25:05 +0530

    Updated phone_details.py
    Added country name and time zones info to the program.

--- Mini_Projects/python/phone_details.py
+++ Mini_Projects/python/phone_details.py
@@ -1,26 +1,37 @@
 import phonenumbers
-from phonenumbers import geocoder, carrier
+from phonenumbers import geocoder, carrier, timezone
 
 #Phone numbers
 num = input("Enter the phone number: ")
 try:
-    phone_number = phonenumbers.parse(num)
+    phone_number = phonenumbers.parse(num, "IN")
+    #phonenumbers.is_valid_number(num)
 except:
     print("Invalid Number")
 
 else:
     #Phone number regions
     try:
-        phone_region = geocoder.description_for_number(phone_number, "en")
+        phone_region = geocoder.description_for_valid_number(phone_number, "en")
+        country = geocoder.country_name_for_number(phone_number, "en")
     except:
         print("Region Not Found")
     else:
         print("\nPhone Number's Region: ", phone_region)
+        print("Origin Country: ", country)
 
     #Phone number service providers
     try:
-        number_provider = carrier.name_for_number(phone_number, "en")
+        number_provider = carrier.name_for_valid_number(phone_number, "en")
     except:
         print("Service Provider Not Found")
     else:
         print("Service Provider: ", number_provider)
+
+    #Phone number time zone
+    try:
+        time_zone = timezone.time_zones_for_number(phone_number)
+    except:
+        print("Time Zone Not Found")
+    else:
+        print("Time Zone: ", time_zone)
