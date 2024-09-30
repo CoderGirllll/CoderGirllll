d9bc257c54cd82f47d4e31b26f6d0fcf9812ebb7
CoderGirllll <131524016+CoderGirllll@users.noreply.github.com>
2024-09-30 23:43:12 +0530

    Pillow Module (Python)
    Display of Images using Pillow Module in Python

---
+++ Lang_Recalls/python/pics.py
@@ -0,0 +1,6 @@
+from PIL import Image
+#from urllib.request import urlopen
+img = Image.open(r"C:\Users\hp\Desktop\backup\Samya's Folder\Scratch 2 Programs\Makhan Chor\Radha Krishn 2.png")
+img.show()
+
+#pic = Image.open(urlopen(r"https://www.google.com/imgres?imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D381275417792065&tbnid=Oxsa6YxknKkwPM&vet=10CBQQxiAoC2oXChMI6Obdj_DqiAMVAAAAAB0AAAAAEFA..i&imgrefurl=https%3A%2F%2Fwww.facebook.com%2Fphoto.php%3Ffbid%3D381275417792065%26id%3D100077288290131%26set%3Da.166924082560534%26locale%3Den_GB&docid=IjphpS3WGUh2AM&w=564&h=564&itg=1&q=krishn&ved=0CBQQxiAoC2oXChMI6Obdj_DqiAMVAAAAAB0AAAAAEFA"))
